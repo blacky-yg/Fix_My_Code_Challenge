bf6d19fe1bd0e58e05d4e07d54b0352577b055b2
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-03 12:45:30 +0100

    resolve square error

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Square class """
+class square():
+    """ Square class """
+    width = 0
+    height = 0
+
+
+    def __init__(self, *args, **kwargs):
+        """ Init method """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def PermiterOfMySquare(self):
+        """ Permiter of the square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ String representation of the square """
+        return "{}/{}".format(self.width, self.height)
+
+if __name__ == "__main__":
+
+    s = square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.PermiterOfMySquare())
