2ede1c39a46541ecc72246feae165fa2c6818b51
Hamidou TESSILIMI <hamidou.tessilimi@epitech.eu>
2023-01-03 14:46:56 +0100

    fix pycodestyle

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -24,6 +24,7 @@ class Square():
         """ String representation of the square """
         return "{}/{}".format(self.width, self.height)
 
+
 if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
